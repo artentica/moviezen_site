c8081f27777658ba9cf5b03335b52c6b542c32b3
Noctisae <fanch.toquer@laposte.net>
2015-01-26 09:08:13 +0100

    BDD

---
+++ moviezen.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 26 Janvier 2015 à 09:06
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `moviezen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `identifiant` text COLLATE utf8_bin NOT NULL,
+  `mdp` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`identifiant`(255))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`identifiant`, `mdp`) VALUES
+('Francois', '$2y$10$qff4SUmbsjqaY10FiZtOpeO071M5xiSL6Ae7gNZDSFI14fhRpITHa'),
+('Turing', '$2y$10$Vji7Q8AI7cIZZCeJ4uFbV.DWCte6HUUNzzpWYwqADs1ewsi8/GpWu');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscrits`
+--
+
+CREATE TABLE IF NOT EXISTS `inscrits` (
+  `nom` text COLLATE utf8_bin NOT NULL,
+  `prenom` text COLLATE utf8_bin NOT NULL,
+  `tel` varchar(10) COLLATE utf8_bin NOT NULL,
+  `mail` varchar(255) COLLATE utf8_bin NOT NULL,
+  `classe` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `inscrits`
+--
+
+INSERT INTO `inscrits` (`nom`, `prenom`, `tel`, `mail`, `classe`) VALUES
+('qcfdsgsg', 'dgdgfdfg', '0605080908', 'essai@isen-bretagne.fr', 'CSI3'),
+('qsejhfbh', 'fqsjbfsbhvbq', '', 'f@ols.fr', 'Cdnsqjhbvc'),
+('Toique', 'dsqijfdsq', '0205060809', 'fanch@isen.fr', 'CIR3'),
+('Toque', 'Franc', '', 'fanto@is.fr', 'CSI3'),
+('TOQUe', 'dqsjhfd', '', 'fto@is.fr', 'CIR3'),
+('dswf;l', 'ds,nqjfnsdz', '', 'ftoque@is.fr', 'csdfsc'),
+('xfgscv', 'svsvsdv', '0505050505', 'ftoque@isen-bretagne.fr', 'CSI3'),
+('ouezfbhjvb', 'fsjdhgfvsddkhbv', '', 'ftoque@isen.fr', 'sdkvnhsdqhvc'),
+('seqfsdf', 'sdfsdf', '0605080905', 'ftoque@laposte.net', 'CSI3'),
+('efndsfnb', 'fdshbfdszbh', '0205050555', 'isen@isen-bretagne.fr', 'CIR2'),
+('adsdqfsf', 'sdfsfd', '0550500606', 'to@isen-bretagne.fr', 'CSI3');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscrits_lots`
+--
+
+CREATE TABLE IF NOT EXISTS `inscrits_lots` (
+  `inscrit_mail` varchar(255) COLLATE utf8_bin NOT NULL,
+  `lots` varchar(2) COLLATE utf8_bin NOT NULL,
+  `date_emprunt` text COLLATE utf8_bin NOT NULL,
+  `date_retour` text COLLATE utf8_bin NOT NULL,
+  KEY `inscri` (`inscrit_mail`),
+  KEY `lot` (`lots`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `inscrits_lots`
+--
+
+INSERT INTO `inscrits_lots` (`inscrit_mail`, `lots`, `date_emprunt`, `date_retour`) VALUES
+('ftoque@isen-bretagne.fr', 'F', '1998-05-05', '1999-05-50'),
+('ftoque@isen-bretagne.fr', 'C', '1998-05-05', '1999-05-50'),
+('ftoque@isen-bretagne.fr', 'D', '1998-05-05', '1999-05-50'),
+('to@isen-bretagne.fr', 'F', '2001-05-06', '2015-05-06'),
+('ftoque@isen-bretagne.fr', 'F', '2015-01-16', '2015-01-09'),
+('ftoque@isen-bretagne.fr', 'D', '2015-01-16', '2015-01-09');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lots`
+--
+
+CREATE TABLE IF NOT EXISTS `lots` (
+  `id` varchar(2) COLLATE utf8_bin NOT NULL,
+  `composition` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `lots`
+--
+
+INSERT INTO `lots` (`id`, `composition`) VALUES
+('C', 'Dolly'),
+('D', 'Micro'),
+('F', 'Camera essai222');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projections`
+--
+
+CREATE TABLE IF NOT EXISTS `projections` (
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `date_release` date NOT NULL,
+  `date_projection` date NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
+  `commentaires` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `projections`
+--
+
+INSERT INTO `projections` (`nom`, `date_release`, `date_projection`, `description`, `commentaires`) VALUES
+('Foxcatcherqedfsdf', '2015-01-14', '2015-01-16', 'Testfzefsdfsf', 'qfsdqfsdfds'),
+('Le magicien d\\''OZ', '2015-01-15', '2015-01-31', 'essafzdfsfsdfc', 'sdfsdgfdsfs'),
+('TESTETS', '2015-01-15', '2015-01-27', 'ESSSSSSSSSSSSSSSSSSSSAI', ''),
+('TTTTTTTTTTTTTTTTTTTTTTTT', '0000-00-00', '0000-00-00', 'TEEEEEEEEEEEEEEEEEE', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projections_inscrits`
+--
+
+CREATE TABLE IF NOT EXISTS `projections_inscrits` (
+  `inscrit_mail` varchar(255) COLLATE utf8_bin NOT NULL,
+  `projection` varchar(255) COLLATE utf8_bin NOT NULL,
+  KEY `inscrit` (`inscrit_mail`),
+  KEY `projection` (`projection`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `projections_inscrits`
+--
+
+INSERT INTO `projections_inscrits` (`inscrit_mail`, `projection`) VALUES
+('fto@is.fr', 'Foxcatcherqedfsdf'),
+('fanto@is.fr', 'Foxcatcherqedfsdf');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `inscrits_lots`
+--
+ALTER TABLE `inscrits_lots`
+  ADD CONSTRAINT `lots_constr` FOREIGN KEY (`lots`) REFERENCES `lots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inscr_mail_constr` FOREIGN KEY (`inscrit_mail`) REFERENCES `inscrits` (`mail`);
+
+--
+-- Contraintes pour la table `projections_inscrits`
+--
+ALTER TABLE `projections_inscrits`
+  ADD CONSTRAINT `proj_constr` FOREIGN KEY (`projection`) REFERENCES `projections` (`nom`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inscr_constr` FOREIGN KEY (`inscrit_mail`) REFERENCES `inscrits` (`mail`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
