9fa5e7780c225c49a62eea3ac0b1ab435c97b269
Artentica <riouallonvincent@gmail.com>
2015-01-30 23:09:43 +0100

    menu resp

---
+++ include/panel-index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '<div id="main-nav-hold">
+            <nav class="main-nav">
+						<a href="index.php" class="nav-btn">Présentation du club</a>
+						<a href="views/cine.php" class="nav-btn">Coté Ciné de l\'ISEN</a>
+                        <a href="views/emprunt.php" class="nav-btn">Emprunt de matériel</a>
+                        <a href="views/admin.php" class="nav-btn">Espace Administrateur</a>
+                        <a href="views/calendrier.php" class="nav-btn">Calendrier des emprunts</a>
+                        <a href="views/projection.php" class="nav-btn">Espace liste de projection</a>
+                        <div class="menu-btn"><a><span></span><span></span><span></span></a></div>
+
+		    </nav>
+        </div>';
+
+?>
--- views/emprunt.php
+++ views/emprunt.php
@@ -33,6 +33,8 @@
 	<link rel="stylesheet" type="text/css" href="../CSS/index.css">
 	<link rel="stylesheet" type="text/css" href="../CSS/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../CSS/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/menu.css">
+
     <link rel="stylesheet" type="text/css" href="../CSS/jquery-ui.structure.min.css">
     <link rel="stylesheet" type="text/css" href="../CSS/jquery-ui.theme.min.css">
     <script src="../js/jquery-2.1.3.min.js"></script>
@@ -46,20 +48,8 @@
 <body>
     <img src="../Images/moviezen2.jpg" alt="bannière" id="banniere"/>
     <header>
-        <nav class="navbar navbar-default">
-  			<div class="container-fluid">
-  				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li><a href="../index.php">Présentation du club</a></li>
-						<li><a href="cine.php">Coté Ciné de l'ISEN</a></li>
-                        <li class="active"><a href="emprunt.php">Emprunt de matériel</a></li>
-					    <li><a href="admin.php">Espace Administrateur</a></li>
-                        <li><a href="calendrier.php">Calendrier des emprunts</a></li>
-                        <li><a href="projection.php">Espace liste de projection</a></li>
-                    </ul>
-				</div>
-			</div>
-		</nav>
+        <?php
+       include '../include/panel-global.php';?>
     </header>
     <div class="panel panel-default">
 		<div class="panel-body">
