f37a65249b511366388ae2942d58a1148c454ecf
Artentica <riouallonvincent@gmail.com>
2015-02-01 11:26:32 +0100

    button deco

--- includes/panel-global.php
+++ includes/panel-global.php
@@ -5,6 +5,8 @@ echo '<div id="main-nav-hold">
 						<a href="../index.php" class="nav-btn">Présentation du club</a>
 						<a href="cine.php" class="nav-btn">Coté Ciné de l\'ISEN</a>
                         <a href="emprunt.php" class="nav-btn">Emprunt de matériel</a>
+                         <button type="button" class="btn btn-danger" aria-label="Left Align">
+  <span class="glyphicon glyphicon-off" aria-hidden="true"></span></button>
                         <a href="admin.php" class="nav-btn">Espace Administrateur</a>
                         <a href="calendrier.php" class="nav-btn">Calendrier des emprunts</a>
                         <a href="projection.php" class="nav-btn">Espace liste de projection</a>
