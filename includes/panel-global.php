a74d97f05e2770bb3113d5ed541f4b7c68c52270
Artentica <riouallonvincent@gmail.com>
2015-02-01 13:03:49 +0100

    function deco2

--- includes/panel-global.php
+++ includes/panel-global.php
@@ -4,10 +4,18 @@ echo '<div id="main-nav-hold">
             <nav class="main-nav">
 						<a href="../index.php" class="nav-btn">Présentation du club</a>
 						<a href="cine.php" class="nav-btn">Coté Ciné de l\'ISEN</a>
-                        <a href="emprunt.php" class="nav-btn">Emprunt de matériel</a>
-                         <button type="button" class="btn btn-danger" aria-label="Left Align">
-  <span class="glyphicon glyphicon-off" aria-hidden="true"></span></button>
-                        <a href="admin.php" class="nav-btn">Espace Administrateur</a>
+                        <a href="emprunt.php" class="nav-btn">Emprunt de matériel</a>';
+
+
+
+if(!empty($_SESSION["authentifie"])){
+    echo'<button title="Deconnexion button" type="button" class="btn" id="decobtn"><a href="?deco=1"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></button>';
+}
+
+
+
+
+                        echo'<a href="admin.php" class="nav-btn">Espace Administrateur</a>
                         <a href="calendrier.php" class="nav-btn">Calendrier des emprunts</a>
                         <a href="projection.php" class="nav-btn">Espace liste de projection</a>
                         <div class="menu-btn" onClick="showMenu()"><a><span></span><span></span><span></span></a></div>
--- includes/panel-index.php
+++ includes/panel-index.php
@@ -4,10 +4,18 @@ echo '<div id="main-nav-hold">
             <nav class="main-nav">
 						<a href="index.php" class="nav-btn">Présentation du club</a>
 						<a href="views/cine.php" class="nav-btn">Coté Ciné de l\'ISEN</a>
-                        <a href="views/emprunt.php" class="nav-btn">Emprunt de matériel</a>
-                        <button type="button" class="btn btn-danger" aria-label="Left Align">
-  <span class="glyphicon glyphicon-off" aria-hidden="true"></span></button>
-                        <a href="views/admin.php" class="nav-btn">Espace Administrateur</a>
+                        <a href="views/emprunt.php" class="nav-btn">Emprunt de matériel</a>';
+
+
+
+if(!empty($_SESSION["authentifie"])){
+    echo'<button title="Deconnexion button" type="button" class="btn" id="decobtn"><a href="?deco=1"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></button>';
+}
+
+
+
+
+                        echo'<a href="views/admin.php" class="nav-btn">Espace Administrateur</a>
                         <a href="views/calendrier.php" class="nav-btn">Calendrier des emprunts</a>
                         <a href="views/projection.php" class="nav-btn">Espace liste de projection</a>
                         <div class="menu-btn" onClick="showMenu()"><a><span></span><span></span><span></span></a></div>
