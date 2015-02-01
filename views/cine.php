c19115db0d8c1da82c8314f610c7c2667c7b1a15
Artentica <riouallonvincent@gmail.com>
2015-02-02 00:14:24 +0100

    a

--- views/cine.php
+++ views/cine.php
@@ -121,13 +121,13 @@ include_once("../includes/function_global.php");
                 echo('<h1 id="inscr">Se désinscrire de la projection</h1>
             <form method="post" action="cine.php" id="form-register">
                 <input type="hidden" name="del_mail" id="del_mail" value="'.$_SESSION["mail"].'"/>
-                <input type="submit" class="inscrval btn btn-danger btn-lg" id="save_cine" value="Se désinscrire"/>
+                <input type="submit" class="button dark_grey" id="save_cine" value="Se désinscrire"/>
             </form>');
                 
             }
 
             ?>
-            
+            <a href="#" class="button dark_grey">Download</a>
 		</div>
 	</div>
     
