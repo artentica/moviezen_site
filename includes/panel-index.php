601f620a2c7d429cd7f896019281e4b211d638b0
artentica <riouallonvincent@gmail.com>
2015-02-02 11:57:36 +0100

    change menu title and responsive

--- includes/panel-index.php
+++ includes/panel-index.php
@@ -1,12 +1,34 @@
 <?php
 
+switch (basename($_SERVER['PHP_SELF'])) {
+    case 'index.php':
+        $page = "Présentation du club";
+        break;
+    case 'cine.php':
+        $page = "Coté Ciné de l'ISEN";
+        break;
+    case 'emprunt.php':
+        $page = "Emprunt de matériel";
+        break;
+    case 'admin.php':
+        $page = "Espace Administrateur";
+        break;
+    case 'calendrier.php':
+        $page = "Calendrier des emprunts";
+        break;
+    case 'projection.php':
+        $page = "Espace liste de projection";
+        break;
+    default:
+}
+
 echo '<div id="main-nav-hold">
             <nav class="main-nav">
 						<a href="index.php" class="nav-btn">Présentation du club</a>
 						<a href="views/cine.php" class="nav-btn">Coté Ciné de l\'ISEN</a>
                         <a href="views/emprunt.php" class="nav-btn">Emprunt de matériel</a>';
 
-
+    echo '<span id="title_page_menu">'.$page.'</span>';
 
 if(!empty($_SESSION["authentifie"])){
     echo'<a  title="Deconnexion button" type="button" class="btn" id="decobtn" href="?deco=1"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a>';
